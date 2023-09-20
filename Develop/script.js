c1a416b442b0d3a719e41a820331e58e8528f044
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-09-20 17:58:32 -0400

    adds current date to header

--- Develop/script.js
+++ Develop/script.js
@@ -1,38 +1,45 @@
 var scheduleInput = document.querySelector("#description");
 var saveButton = document.querySelector("#saveBtn");
+var currentDayEl = document.querySelector("#currentDay");
+// var date = dayjs();
+// console.log(date);
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  renderSavedSchedule();
+var date = dayjs().format("dddd MMMM, D");
+$("#currentDay").text(date);
 
-  function displayMessage(type, message) {
-    msgDiv.textContent = message;
-    msgDiv.setAttribute("class", type);
-  }
+// $(function
 
-  function renderSavedSchedule() {
-    var textInput = localStorage.getItem("text");
+// TODO: Add a listener for click events on the save button. This code should
+// use the id in the containing time-block as a key to save the user input in
+// local storage. HINT: What does `this` reference in the click listener
+// function? How can DOM traversal be used to get the "hour-x" id of the
+// time-block containing the button that was clicked? How might the id be
+// useful when saving the description in local storage?
+//
 
-    textInput.textContent = text;
-  }
+// function displayMessage(type, message) {
+//   msgDiv.textContent = message;
+//   msgDiv.setAttribute("class", type);
+// }
 
-  saveButton.addEventListener("click", function (event) {
-    event.preventDefault();
+// function renderSavedSchedule() {
+//   var textInput = localStorage.getItem("text");
 
-    var schedule = emailInput.value;
+//   textInput.textContent = text;
+// }
 
-    localStorage.setItem("text", schedule);
-    renderSavedSchedule();
-  });
-});
+//   saveButton.addEventListener("click", function (event) {
+//     event.preventDefault();
+
+//     var schedule = scheduleInputInput.value;
+//     console.log(scheduleInput.value);
+
+//     localStorage.setItem("text", schedule);
+//     renderSavedSchedule();
+//   });
+// });
 
 // function getApi() {
 //   var requestUrl = "";
@@ -59,4 +66,4 @@ $(function () {
 // TODO: Add code to display the current date in the header of the page.
 
 // });
-// fetchButton.addEventListener("click", getApi);
+// fetchButton.addEventListener("click", getApi)
